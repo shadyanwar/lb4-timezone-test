2be503d6317b909e0e892187e536bbab76856a65
Sh <sh>
2019-08-13 23:51:40 +0200

    Apply a default TZ to node start script

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -10,20 +10,38 @@ export class UserController {
   constructor(@repository(UserRepository)
   private UserRepository: UserRepository, ) { }
 
+  @get('/users/{id}') async getUserById(@param.path.number('id') id: number): Promise<Object> {
+    let result = await this.UserRepository.findById(id);
+    let newResult = {
+      id: result.id,
+      name: result.name,
+      dateAdded: moment(result.dateAdded).format('YYYY-MM-DDTHH:mm:ss.SSZ'),
+      // dateAdded: result.dateAdded.toLocaleDateString()
+      //             + ' '
+      //             + result.dateAdded.toLocaleTimeString(),
+    }
+
+    return newResult;
+  }
+
   @get('/users') async getUsers(): Promise<Object> {
-    return this.UserRepository.find()
+    return await this.UserRepository.find()
   }
   @get('/addUser') async addUser(
     @param.query.string('name') name: string,
   ): Promise<Object> {
 
-    const dateTimeNowTz = moment(new Date())
-      .clone().tz('Asia/Tokyo')
-      .format('YYYY-MM-DDTHH:mm:ss.SSZ');
+    const dateTimeNowTz = moment(new Date()).format('YYYY-MM-DDTHH:mm:ss.SSZ');
+    // const dateTimeNowTz = new Date().toLocaleDateString()
+    //                       + ' '
+    //                       + new Date().toLocaleTimeString();
 
     const createdUser = await this.UserRepository.create({
       name: name,
-      dateAdded: dateTimeNowTz,
+      dateAdded: moment(new Date()).format('YYYY-MM-DDTHH:mm:ss.SSZ'),
+      // dateAdded: new Date().toLocaleDateString()
+      //             + ' '
+      //             + new Date().toLocaleTimeString(),
     });
     return {
       createdUser: createdUser,
